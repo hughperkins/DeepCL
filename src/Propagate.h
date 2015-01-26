2ab53d269e459002b898a58cee2cf05d944ae449
Hugh Perkins <hughperkins@gmail.com>
2015-01-27 07:38:44 +0800

    windows build

--- src/Propagate.h
+++ src/Propagate.h
@@ -1,5 +1,12 @@
+// Copyright Hugh Perkins 2014 hughperkins at gmail
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
+
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 
