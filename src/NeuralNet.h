9021945bc728bcb1cd1a723fe8c7d439610d2a19
Hugh Perkins <hughperkins@gmail.com>
2015-01-27 00:27:07 +0800

    start looking at adding exports for windows

--- src/NeuralNet.h
+++ src/NeuralNet.h
@@ -23,7 +23,17 @@ class LayerMaker;
 #define VIRTUAL virtual
 #define STATIC static
 
-class NeuralNet {
+#if defined(_WIN32) 
+# if defined(ClConvolve_EXPORTS)
+#  define ClConvolve_EXPORT __declspec(dllexport)
+# else
+#  define ClConvolve_EXPORT __declspec(dllimport)
+# endif // ClConvolve_EXPORTS
+#else // _WIN32
+# define ClConvolve_EXPORT
+#endif
+
+class ClConvolve_EXPORT NeuralNet {
 public:
     std::vector< Layer *> layers;
     OpenCLHelper *cl;
