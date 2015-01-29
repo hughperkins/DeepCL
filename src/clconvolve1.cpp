45d0d2f1656ff5b7b70f7eccfd8c2ca1654c7303
Hugh Perkins <hughperkins@gmail.com>
2015-01-29 21:16:47 +0800

    normalization

--- src/clconvolve1.cpp
+++ src/clconvolve1.cpp
@@ -141,7 +141,7 @@ void go(Config config) {
     float stdDev;
     if( config.normalization == "stddev" ) {
         NormalizationHelper::getMeanAndStdDev( trainData, Ntrain * inputCubeSize, &mean, &stdDev );
-    } else if( config.normalization == "minmax" ) {
+    } else if( config.normalization == "maxmin" ) {
         NormalizationHelper::getMinMax( trainData, Ntrain * inputCubeSize, &mean, &stdDev );
     } else {
         cout << "Error: Unknown normalization: " << config.normalization << endl;
