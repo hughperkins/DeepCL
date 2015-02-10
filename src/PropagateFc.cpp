329ff57ea4dbc2becee6f199f31af4fd06af71b7
Hugh Perkins <hughperkins@gmail.com>
2015-02-10 19:35:16 +0800

    try differnet workgroupsize

--- src/PropagateFc.cpp
+++ src/PropagateFc.cpp
@@ -64,8 +64,10 @@ VIRTUAL void PropagateFc::propagate( int batchSize, CLWrapper *dataWrapper, CLWr
         ->in( dim.filterSize )
         ->in( results1Wrapper )->out( results2Wrapper );
     int maxglobalId = batchSize * dim.numFilters * dim.numInputPlanes;
-    numWorkgroups = ( maxglobalId + maxWorkgroupSize - 1 ) / maxWorkgroupSize;
-    kernel_reduce->run_1d( numWorkgroups * maxWorkgroupSize, maxWorkgroupSize );
+//    numWorkgroups = ( maxglobalId + maxWorkgroupSize - 1 ) / maxWorkgroupSize;
+//    kernel_reduce->run_1d( numWorkgroups * maxWorkgroupSize, maxWorkgroupSize );
+    numWorkgroups = ( maxglobalId + 32 - 1 ) / 32;
+    kernel_reduce->run_1d( numWorkgroups * 32, 32 );
     cl->finish();
     StatefulTimer::timeCheck("PropagateFc::propagate after reduce1");
 
