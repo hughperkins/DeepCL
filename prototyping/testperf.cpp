8af6fbc4a7a8bed17033b6bda2331ecbb6057345
Hugh Perkins <hughperkins@gmail.com>
2015-02-19 17:26:18 +0700

    testperf

--- prototyping/testperf.cpp
+++ prototyping/testperf.cpp
@@ -17,8 +17,8 @@ int main( int argc, char *argv[] ) {
     string kernelSource = R"DELIM(
         kernel void test(global float *stuff) {
             float a = stuff[get_global_id(0)];
-            float b = stuff[1];
-            float c = stuff[2];
+            float b = stuff[1024];
+            float c = stuff[1025];
             #pragma unroll 100
             for( int i = 0; i < N_ITERATIONS; i++ ) {
                 a = a * b + c;
@@ -35,8 +35,8 @@ int main( int argc, char *argv[] ) {
     for( int i = 0; i < 2048; i++ ) {
         stuff[i] = 2.0f;
     }
-    stuff[1] = 2.0f;
-    stuff[2] = -2.0f;
+    stuff[1024] = 2.0f;
+    stuff[1025] = -2.0f;
     kernel->inout( 2048, stuff );
     kernel->run_1d( workgroupSize, workgroupSize );
     cl->finish();
