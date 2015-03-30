86688e4a3d587e8816479fb304a4fd10bd80c45f
Hugh Perkins <hughperkins@gmail.com>
2015-03-30 23:17:04 +0800

    add comment at top of clconvolve-fixedweights.cpp

--- prototyping/clconvolve-fixedweights.cpp
+++ prototyping/clconvolve-fixedweights.cpp
@@ -4,6 +4,9 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can 
 // obtain one at http://mozilla.org/MPL/2.0/.
 
+// this initializes the weights using a fixed seed, repeatably
+// to enable comparison with convnetjs
+
 
 #include <iostream>
 #include <algorithm>
