9adc8f6c1bf6c1b1ae30795b81ea7b62c872fcff
Hugh Perkins <hughperkins@gmail.com>
2015-04-06 20:05:14 +0800

    update the python example files to point direclty at mnist files, rather than needing conversion to norb format first

--- python/test_deepcl.py
+++ python/test_deepcl.py
@@ -12,7 +12,7 @@ if len(sys.argv) != 2:
     print('usage: python ' + sys.argv[0] + ' [mnist data directory (containing the .mat files)]')
     sys.exit(-1)
 
-mnistFilePath = sys.argv[1] + '/t10k-dat.mat' 
+mnistFilePath = sys.argv[1] + '/t10k-images-idx3-ubyte' 
 
 net = PyDeepCL.NeuralNet(1,28)
 print('created net')
--- python/test_lowlevel.py
+++ python/test_lowlevel.py
@@ -11,7 +11,7 @@ if len(sys.argv) != 2:
     print('usage: python ' + sys.argv[0] + ' [mnist data directory (containing the .mat files)]')
     sys.exit(-1)
 
-mnistFilePath = sys.argv[1] + '/t10k-dat.mat' 
+mnistFilePath = sys.argv[1] + '/t10k-images-idx3-ubyte' 
 
 net = PyDeepCL.NeuralNet()
 net.addLayer( PyDeepCL.InputLayerMaker().numPlanes(1).imageSize(28) )
