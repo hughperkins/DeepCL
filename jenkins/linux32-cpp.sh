2b0268753d5cf12903132abbb7a9cfd38d5a2a7e
Hugh Perkins <hughperkins@gmail.com>
2015-04-10 21:44:58 +0800

    typo in linux32-cpp.sh

--- jenkins/linux32-cpp.sh
+++ jenkins/linux32-cpp.sh
@@ -5,5 +5,5 @@ mkdir -p build
 cd build
 schroot -c trusty_i386 -- cmake ..
 schroot -c trusty_i386 -- make
-tar -cjf deepcl-linux64-${version}.tar.bz2 --exclude=CMake* --exclude=CMakeFiles --exclude=cmake* --exclude=Makefile --exclude=*.png --exclude=*.dat *
-
+tar -cjf deepcl-linux32-${version}.tar.bz2 --exclude=CMake* --exclude=CMakeFiles --exclude=cmake* --exclude=Makefile --exclude=*.png --exclude=*.dat *
+    
