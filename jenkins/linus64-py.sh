bf0ed5c042e0004ff20542b1083128c6733e70ed
Hugh Perkins <hughperkins@gmail.com>
2015-04-10 08:09:14 +0800

    linux64-27

---
+++ jenkins/linus64-py.sh
@@ -0,0 +1,20 @@
+echo args: $1
+
+pyenv=$1
+echo pyenv: $pyenv
+
+pwd
+cd python
+pwd
+rm -Rf dist mysrc build PyDeepCL.cpp
+ls
+. $HOME/env-2.7/bin/activate
+pwd
+pip install cython pypandoc || exit 1
+python setup.py build_ext -i || exit 1
+python setup.py bdist_egg || exit 1
+
+# just ignore the error on next line for now (if already uploaded this version)
+python setup.py bdist_egg upload
+exit 0
+
---
+++ jenkins/linux-64-py27.sh
@@ -0,0 +1,4 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+bash $DIR/linux64-py.sh env-2.7 || exit 1
+
