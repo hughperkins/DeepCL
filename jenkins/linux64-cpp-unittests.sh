799fed01c5e4c1e714aed09f67d7360dc12eb8ed
Hugh Perkins <hughperkins@gmail.com>
2015-04-18 21:44:55 +0800

    add linux64 cpp unittests.sh for jenkins

---
+++ jenkins/linux64-cpp-unittests.sh
@@ -0,0 +1,10 @@
+pwd
+version=$(cat jenkins/version.txt)
+rm -Rf build
+mkdir -p build
+cd build
+cmake -D BUILD_PYTHON_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF ..
+make -j 8
+./unittests
+# tar -cjf deepcl-linux64-${version}.tar.bz2 --exclude=CMake* --exclude=CMakeFiles --exclude=cmake* --exclude=Makefile --exclude=*.png --exclude=*.dat *
+
