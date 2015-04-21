71806abf01c38f3d7c096408314c48cd52a40efd
Hugh Perkins <hughperkins@gmail.com>
2015-04-21 08:16:12 +0800

    luarocks fix package slightly

--- lua/pack.sh
+++ lua/pack.sh
@@ -3,25 +3,26 @@
 # this can be run on linux, to create the rock
 # at least, that is the goal
 
-export package=DeepCL
+export package=LuaDeepCL
+export packagelower=luadeepcl
 export version=$(head -n 1 version.txt)
-echo package=${package} version=${version}
+echo packagelower=${packagelower} version=${version}
 
 rm *.rock
 rm *.rockspec
 
 rm -Rf dist/*
 mkdir -p dist
-mkdir -p dist/${package}-${version}
-mkdir -p dist/${package}-${version}/pkgsrc
-rsync -av ../src/ dist/${package}-${version}/pkgsrc/src/
-cp *.lua *.i CMakeLists.txt *.cxx dist/${package}-${version}/
-touch dist/${package}-${version}/inpkg.flag
-(cd dist; tar czvpf ${package}-${version}.tar.gz ${package}-${version}/)
+mkdir -p dist/${packagelower}-${version}
+mkdir -p dist/${packagelower}-${version}/pkgsrc
+rsync -av ../src/ dist/${packagelower}-${version}/pkgsrc/src/
+cp *.lua *.i CMakeLists.txt *.cxx dist/${packagelower}-${version}/
+touch dist/${packagelower}-${version}/inpkg.flag
+(cd dist; tar czvpf ${package}-${version}.tar.gz ${packagelower}-${version}/)
 
-cp rockspec ${package}-${version}.rockspec
-sed -i -e "s/^version = \".*$/version = \"${version}\"/" ${package}-${version}.rockspec
+cp rockspec ${packagelower}-${version}.rockspec
+sed -i -e "s/^version = \".*$/version = \"${version}\"/" ${packagelower}-${version}.rockspec
 
-luarocks pack ${package}-${version}.rockspec
+luarocks pack ${packagelower}-${version}.rockspec
 
 
