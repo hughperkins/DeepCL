bae5722d635a90b188bb4d85bc6e5c4b9d03f77a
Hugh Perkins <hughperkins@gmail.com>
2015-05-04 22:31:13 +0800

    add some more doc to ConvolutionalLayer.cpp

--- src/ConvolutionalLayer.cpp
+++ src/ConvolutionalLayer.cpp
@@ -1,4 +1,17 @@
 // this has moved to:
 // conv/ConvolutionalLayer.cpp, ie
 // https://github.com/hughperkins/DeepCL/blob/master/src/conv/ConvolutionalLayer.cpp
+//
+// but really if you're after the convolutional implementations themselves...
+//
+// opencl code is at:
+// https://github.com/hughperkins/DeepCL/blob/master/cl/
+// - in particular forward convolution is in the files called forward[something].cl
+// - backward convolution is in the files called backward[something].cl
+// - weights gradients calculations are in the files called backpropweights[something].cl
+//
+// c++ code that runs these files is at https://github.com/hughperkins/DeepCL/blob/master/src/conv
+// - forward prop in the files called Forward[something].h (and .cpp)
+// - backward prop in the files called Backward[something].h (and .cpp)
+// - weights gradient calculations are in the files called BackpropWeights[something].h (and .cpp)
 
