806f02703a07d68c91049e6201d3c25a8ee0991d
Hugh Perkins <hughperkins@gmail.com>
2015-05-04 22:38:18 +0800

    add some more doc to ConvolutionalLayer.cpp

--- src/ConvolutionalLayer.cpp
+++ src/ConvolutionalLayer.cpp
@@ -15,3 +15,14 @@
 // - backward prop in the files called Backward[something].h (and .cpp)
 // - weights gradient calculations are in the files called BackpropWeights[something].h (and .cpp)
 
+// quick summary of the different forward implementations:
+//
+// forward1: generic forward propagation, should probably work on most configurations, but
+// doesnt use local memory, so slow
+// forward2,3,4, etc ...: various configurations of using local memory to reduce transfers
+//  from gpu global memory.  Each configuration will only work for certain image sizes etc
+//  and so we need different implementations for different sizes
+//  Up till now, most of the work has been going into go-board sized images, 19x19, and also
+//  a bit of work into mnist-sized, around 28x28 or 24x24
+//  Larger images probably need some kind of blocking structure most likely
+
