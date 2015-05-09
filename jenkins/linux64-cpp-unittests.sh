b949939bea6d9ee993a7955a59851ce795673f56
Hugh Perkins <hughperkins@gmail.com>
2015-05-09 12:54:29 +0800

    add xml results to jenkins unittests

--- jenkins/linux64-cpp-unittests.sh
+++ jenkins/linux64-cpp-unittests.sh
@@ -5,6 +5,6 @@ mkdir -p build
 cd build
 cmake -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF ..
 make -j 8
-./unittests
+./unittests --gtest_output="xml:testresults.xml"
 # tar -cjf deepcl-linux64-${version}.tar.bz2 --exclude=CMake* --exclude=CMakeFiles --exclude=cmake* --exclude=Makefile --exclude=*.png --exclude=*.dat *
 
