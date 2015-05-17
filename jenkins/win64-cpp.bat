9a784df348b619cf15991aba2f4acf6d43b768e7
Hugh Perkins <hughperkins@gmail.com>
2015-05-17 22:06:52 +0800

    update jenkins for win32 for libjpegs-turbo download

--- jenkins/win64-cpp.bat
+++ jenkins/win64-cpp.bat
@@ -3,12 +3,21 @@ for /f "" %%i in (version.txt) do (
    set version=%%i
 )
 cd ..
+
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('http://deepcl.hughperkins.com/Downloads/turbojpeg-1.4.0-win64-static.zip', 'turbojpeg-win64.zip')
+if errorlevel 1 exit /B 1
+mkdir turbojpeg-win64
+cd turbojpeg-win64
+"c:\program files\7-Zip\7z.exe" x ..\turbojpeg-win64.zip
+if errorlevel 1 exit /B 1
+
+
 dir
 rmdir /s /q build
 mkdir build
 cd build
 dir
-"c:\program files (x86)\cmake\bin\cmake" -G "Visual Studio 10 2010 Win64" -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF ..
+"c:\program files (x86)\cmake\bin\cmake" -G "Visual Studio 10 2010 Win64" -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF -D JPEG_INCLUDE_DIR=%CD%\..\turbojpeg-win64 -D JPEG_LIBRARY=%CD%\..\turbojpeg-win64\turbojpeg-static.lib ..
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ALL_BUILD.vcxproj /p:Configuration=Release
 if errorlevel 1 exit /B 1
 
