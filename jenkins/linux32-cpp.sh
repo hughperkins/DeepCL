b5ba342baf9feeeba0b6433a9c40c9a82e48017b
Hugh Perkins <hughperkins@gmail.com>
2015-05-17 21:15:11 +0800

    update jenkins jobs to fail on failure, maybe

--- jenkins/linux32-cpp.sh
+++ jenkins/linux32-cpp.sh
@@ -3,7 +3,7 @@ version=$(cat jenkins/version.txt)
 rm -Rf build
 mkdir -p build
 cd build
-schroot -c trusty_i386 -- cmake -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF ..
-schroot -c trusty_i386 -- make
+schroot -c trusty_i386 -- cmake -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF .. || exit 1
+schroot -c trusty_i386 -- make || exit 1
 tar -cjf deepcl-linux32-${version}.tar.bz2 --exclude=CMake* --exclude=CMakeFiles --exclude=cmake* --exclude=Makefile --exclude=*.png --exclude=*.dat *
-    
+
--- jenkins/linux64-cpp.sh
+++ jenkins/linux64-cpp.sh
@@ -3,7 +3,7 @@ version=$(cat jenkins/version.txt)
 rm -Rf build
 mkdir -p build
 cd build
-cmake -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF ..
-make
+cmake -D BUILD_PYSWIG_WRAPPERS:BOOL=OFF -D BUILD_LUA_WRAPPERS:BOOL=OFF .. || exit 1
+make || exit 1
 tar -cjf deepcl-linux64-${version}.tar.bz2 --exclude=CMake* --exclude=CMakeFiles --exclude=cmake* --exclude=Makefile --exclude=*.png --exclude=*.dat *
 
