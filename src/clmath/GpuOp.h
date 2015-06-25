f85b3584b93bf716f69a90c9ecfe766f4853abc3
Hugh Perkins <hughperkins@gmail.com>
2015-06-25 19:35:23 +0800

    fixed windows build maybe

--- src/clmath/GpuOp.h
+++ src/clmath/GpuOp.h
@@ -15,66 +15,68 @@ class EasyCL;
 class CLWrapper;
 class CLKernel;
 
+#include "DeepCLDllExport.h"
+
 #define VIRTUAL virtual
 #define STATIC static
 
-class Op1 {
+class DeepCL_EXPORT Op1 {
 public:
     virtual std::string getOperationString() = 0;
     virtual std::string getName() = 0;
 };
-class Op1Equal : public Op1 {
+class DeepCL_EXPORT Op1Equal : public Op1 {
     std::string getOperationString() {
         return "val_one";
     }
     std::string getName(){ return "Op1_Equal"; }
 };
-class Op1Inv : public Op1 {
+class DeepCL_EXPORT Op1Inv : public Op1 {
     std::string getOperationString() {
         return "1.0f / val_one";
     }
     std::string getName(){ return "Op1_Inv"; }
 };
-class Op1Sqrt : public Op1 {
+class DeepCL_EXPORT Op1Sqrt : public Op1 {
     std::string getOperationString() {
         return "native_sqrt( val_one )";
     }
     std::string getName(){ return "Op1_Sqrt"; }
 };
-class Op1Squared : public Op1 {
+class DeepCL_EXPORT Op1Squared : public Op1 {
     std::string getOperationString() {
         return "val_one * val_one";
     }
     std::string getName(){ return "Op1_Squared"; }
 };
 
-class Op2 {
+class DeepCL_EXPORT Op2 {
 public:
     virtual std::string getOperationString() = 0;
     virtual std::string getName() = 0;
 };
-class Op2Add : public Op2 {
+class DeepCL_EXPORT Op2Add : public Op2 {
 public:
     std::string getOperationString() {
         return "val_one + val_two";
     }
     std::string getName(){ return "Op2_Add"; }
 };
-class Op2Mul : public Op2 {
+class DeepCL_EXPORT Op2Mul : public Op2 {
 public:
     std::string getOperationString() {
         return "val_one * val_two";
     }
     std::string getName(){ return "Op2_Mul"; }
 };
-class Op2Sub : public Op2 {
+class DeepCL_EXPORT Op2Sub : public Op2 {
 public:
     std::string getOperationString() {
         return "val_one - val_two";
     }
     std::string getName(){ return "Op2_Sub"; }
 };
-class Op2Div : public Op2 {
+class DeepCL_EXPORT Op2Div : public Op2 {
 public:
     std::string getOperationString() {
         return "val_one / val_two";
@@ -85,7 +87,7 @@ public:
 
 // use to update one buffer by adding another buffer, in-element
 // not thread-safe
-class GpuOp {
+class DeepCL_EXPORT GpuOp {
 public:
     EasyCL *cl; // NOT belong to us, dont delete
     CLKernel *kernel;
