5f5e4adbb4655d331901c25918de498514b783cd
Hugh Perkins <hughperkins@gmail.com>
2015-08-19 16:41:14 +0800

    update activate.sh create setup.py

--- src/activate.sh
+++ src/activate.sh
@@ -5,5 +5,7 @@ dist_dir=$(dirname ${bin_dir})
 
 export PATH=${bin_dir}:$PATH
 export LD_LIBRARY_PATH=${dist_dir}/lib:${LD_LIBRARY_PATH}
-export PYTHONPATH=${dist_dir}/lib:${PYTHONPATH}
+#export PYTHONPATH=${dist_dir}/lib:${PYTHONPATH}
+export CPATH=${dist_dir}/include:${dist_dir}/include/easycl:${dist_dir}/include/deepcl:${CPATH}
+export LDFLAGS="-L${dist_dir}/lib ${LDFLAGS}"
 
