cd35d2f69f59f53c8b584796678bd4c416ee7aad
Hugh Perkins <hughperkins@gmail.com>
2015-08-21 06:39:26 +0800

    improve activate.bat to incorporate LIB and CL vars

--- src/activate.bat
+++ src/activate.bat
@@ -1,3 +1,3 @@
 set "PATH=%~dp0.;%PATH%"
-set "PYTHONPATH=%~dp0.;%PYTHONPATH%"
-
+set "CL=%CL% /I%~dp0..\include /I%~dp0..\include\deepcl /I%~dp0..\include\easycl"
+set "LIB=%~dp0..\lib;%~dp0..\lib\import %LIB%"
