891946b6cbaebb8c00307e211f89bac35b16460a
Hugh Perkins <hughperkins@gmail.com>
2015-08-25 08:34:52 +0800

    try to fix linux tar versioning in jenkins build

--- jenkins/linux32-cpp.sh
+++ jenkins/linux32-cpp.sh
@@ -1,3 +1,7 @@
+version=$(cat jenkins/version.txt)
+echo version ${version}
 schroot -c trusty_i386 bash jenkins/linux-cpp.sh || exit 1
+echo version ${version}
 tar -cjf deepcl-linux32-${version}.tar.bz2 dist
+tar -tf deepcl-linux64-${version}.tar.bz2
 
--- jenkins/linux64-cpp.sh
+++ jenkins/linux64-cpp.sh
@@ -1,3 +1,7 @@
+version=$(cat jenkins/version.txt)
+echo version ${version}
 bash jenkins/linux-cpp.sh
+echo version ${version}
 tar -cjf deepcl-linux64-${version}.tar.bz2 dist
+tar -tf deepcl-linux64-${version}.tar.bz2
 
