8dbe9f9fe9a4838f62318ddbd476d1010f2c8365
Hugh Perkins <hughperkins@gmail.com>
2015-08-26 08:09:17 +0800

    flake8 on test_deepcl.py

--- python/test_deepcl.py
+++ python/test_deepcl.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-
-#from array import array
 import array
 import PyDeepCL
 import sys
 print('imports done')
 
 if len(sys.argv) != 2:
-    print('usage: python ' + sys.argv[0] + ' [mnist data directory (containing the .mat files)]')
+    print(
+        'usage: python ' + sys.argv[0] +
+        ' [mnist data directory (containing the .mat files)]')
     sys.exit(-1)
 
-mnistFilePath = sys.argv[1] + '/t10k-images-idx3-ubyte' 
+mnistFilePath = sys.argv[1] + '/t10k-images-idx3-ubyte'
 
 cl = PyDeepCL.DeepCL()
 
@@ -22,37 +22,34 @@ print('local memory size, KB:', cl.getLocalMemorySizeKB())
 print('max workgroup size:', cl.getMaxWorkgroupSize())
 print('max alloc size MB:', cl.getMaxAllocSizeMB())
 
-# (N,planes,size) = PyDeepCL.GenericLoader.getDimensions(mnistFilePath)
-
-net = PyDeepCL.NeuralNet(cl, 1,28)
+net = PyDeepCL.NeuralNet(cl, 1, 28)
 print('created net')
-print( net.asString() )
+print(net.asString())
 print('printed net')
-net.addLayer( PyDeepCL.NormalizationLayerMaker().translate(-0.5).scale(1/255.0) )
+net.addLayer(PyDeepCL.NormalizationLayerMaker().translate(-0.5).scale(1/255.0))
 print('added layer ')
-PyDeepCL.NetdefToNet.createNetFromNetdef( net, "rt2-8c5z-relu-mp2-16c5z-relu-mp3-150n-tanh-10n" ) 
-print( net.asString() )
- 
-(N,planes,size) = PyDeepCL.GenericLoader.getDimensions(mnistFilePath)
-print( (N,planes,size) )
+PyDeepCL.NetdefToNet.createNetFromNetdef(
+    net, "rt2-8c5z-relu-mp2-16c5z-relu-mp3-150n-tanh-10n")
+print(net.asString())
+
+(N, planes, size) = PyDeepCL.GenericLoader.getDimensions(mnistFilePath)
+print((N, planes, size))
 
 N = 1280
-images = array.array( 'f', [0] * (N*planes*size*size) )
-labels = array.array('i',[0] * N )
-PyDeepCL.GenericLoader.load(mnistFilePath, images, labels, 0, N )
+images = array.array('f', [0] * (N * planes * size * size))
+labels = array.array('i', [0] * N)
+PyDeepCL.GenericLoader.load(mnistFilePath, images, labels, 0, N)
 print('loaded data')
 
-sgd = PyDeepCL.SGD(cl, 0.002, 0.0 )
+sgd = PyDeepCL.SGD(cl, 0.002, 0.0)
 print('created SGD')
 sgd.setWeightDecay(0.0001)
 netLearner = PyDeepCL.NetLearner(
     sgd, net,
     N, images, labels,
     N, images, labels,
-    128 )
+    128)
 print('created netLearner')
-netLearner.setSchedule( 12 )
+netLearner.setSchedule(12)
 netLearner.run()
 print('done, cleaning up...')
- 
-
