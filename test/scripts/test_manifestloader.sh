31a5e7ddde3a8d52954316062b1b3e1db76c1c3c
Hugh Perkins <hughperkins@gmail.com>
2016-07-25 06:06:33 +0800

    add mnist-to-jpegs to installable files

--- test/scripts/test_manifestloader.sh
+++ test/scripts/test_manifestloader.sh
@@ -13,13 +13,15 @@ if [[ -d /tmp/testmanifest ]]; then {
 
 mkdir /tmp/testmanifest
 
-./mnist-to-jpegs ../data/mnist/train-images-idx3-ubyte /tmp/testmanifest 1280
-./deepcl_train datadir=/tmp/testmanifest trainfile=manifest.txt validatefile=manifest.txt numtrain=1280 numtest=1280 learningrate=0.002 numepochs=3
+source ../dist/bin/activate.sh
+
+mnist-to-jpegs ../data/mnist/train-images-idx3-ubyte /tmp/testmanifest 1280
+deepcl_train datadir=/tmp/testmanifest trainfile=manifest.txt validatefile=manifest.txt numtrain=1280 numtest=1280 learningrate=0.002 numepochs=3
 
 sed -i -e "s%/tmp/testmanifest/%%g" /tmp/testmanifest/manifest.txt
-./deepcl_train datadir=/tmp/testmanifest trainfile=manifest.txt validatefile=manifest.txt numtrain=1280 numtest=1280 learningrate=0.002 numepochs=3
+deepcl_train datadir=/tmp/testmanifest trainfile=manifest.txt validatefile=manifest.txt numtrain=1280 numtest=1280 learningrate=0.002 numepochs=3
 
 head -n 1 /tmp/testmanifest/manifest.txt > /tmp/testmanifest/test.txt
 tail -n +2 /tmp/testmanifest/manifest.txt | awk '{print $1}' >> /tmp/testmanifest/test.txt
-./deepcl_predict writelabels=1 inputfile=/tmp/testmanifest/test.txt outputfile=/tmp/testmanifest/out.txt
+deepcl_predict writelabels=1 inputfile=/tmp/testmanifest/test.txt outputfile=/tmp/testmanifest/out.txt
 
