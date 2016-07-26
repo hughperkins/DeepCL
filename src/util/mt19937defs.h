ac13fbd07dcf2f89cfb74ef075e7e7ba1578ded3
Hugh Perkins <hughperkins@gmail.com>
2016-07-26 14:26:51 +0800

    switch mt19937 back to tr1, so builds in python msvc2008...

--- src/util/mt19937defs.h
+++ src/util/mt19937defs.h
@@ -3,10 +3,10 @@
 #include <random>
 
 //#if (_MSC_VER == 1500 || _MSC_VER == 1600  )
-//#ifdef _MSC_VER // make consistent across all msvc versions, so dont have to retest on different msvc versions...
-//#define TR1RANDOM
-//typedef std::tr1::mt19937 MT19937;
-//#else
+#ifdef _MSC_VER // make consistent across all msvc versions, so dont have to retest on different msvc versions...
+#define TR1RANDOM
+typedef std::tr1::mt19937 MT19937;
+#else
 typedef std::mt19937 MT19937;
-//#endif
+#endif
 
