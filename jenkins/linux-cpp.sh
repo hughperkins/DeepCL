2f469c1c801858dceb8599dc31a9c63a0edb7eba
Hugh Perkins <hughperkins@gmail.com>
2016-07-31 07:41:20 +0800

    just run some tests, in jenkins

--- jenkins/linux-cpp.sh
+++ jenkins/linux-cpp.sh
@@ -8,7 +8,7 @@ cd build
 /usr/bin/cmake .. || exit 1
 make -j 4 install || exit 1
 if [[ $(uname -p) == x86_64 ]]; then {
-    ./deepcl_unittests || exit 1
+    ./deepcl_unittests tests=testforward* || exit 1
 } fi
 cd ..
 
